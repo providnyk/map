e5d60a41a747d2d2e525587735f699be0b58724b
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-01-14 17:39:51 +0200

    0.65.0+a014:2 guest DEL cleanup for Welcome
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_major'         => '64', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
+    'version_major'         => '65', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
     'version_patch'         => '0',
     'version_stage'         => 'a', # a=alfa,b=beta,rc=candidate,r=release
     'version_day'           => '014',
@@ -19,7 +19,7 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'guest'                 => '0.00.0',
+    'guest'                 => '0.01.0',
     'user'                  => '0.64.0',
     'unit'                  => '0.00.0',
     'api'                   => '0.00.0',
