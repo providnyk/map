8ea0e3ec8495bd98d7fa68a9d8e38706181703af
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-01-22 11:57:54 +0200

    0.68.3+a022:4 guest FIX email field value
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- Modules/ProvidnykV1/Resources/views/guest/_email.blade.php
+++ Modules/ProvidnykV1/Resources/views/guest/_email.blade.php
@@ -3,10 +3,13 @@
 										{!! trans('user/form.field.email') !!}
 									</span>
 									<span class="value">
-										<input type="email" class="form-control" placeholder="" name="email" value="{{ $email ?:
-										(old('email')
-											? old('email')
-											: isset($user) ? $user->email : ''
-										) }}">
+										<input type="email" class="form-control" placeholder="" name="email" value="{{
+										isset($email)
+										? $email
+											: (old('email')
+												? old('email')
+												: isset($user) ? $user->email : ''
+												)
+										}}">
 									</span>
 								</div>
--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 $a_production = [
     'version_release'       => '0',
     'version_major'         => '68', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '2',
+    'version_patch'         => '3',
     'version_stage'         => 'a', # a=alfa,b=beta,rc=candidate,r=release
     'version_day'           => '022',
-    'version_seq'           => '3',
+    'version_seq'           => '4',
 ];
 
 return (object) [
@@ -19,7 +19,7 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'guest'                 => '0.04.2',
+    'guest'                 => '0.04.3',
     'user'                  => '0.64.0',
     'unit'                  => '0.00.0',
     'api'                   => '0.00.0',
