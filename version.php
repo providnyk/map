f20fa37af1e2c6ea6ef64c3772cb7f8a23b6bf18
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-03-07 23:29:31 +0200

    0.82.1+a067:2 user FIX versioning by branch
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 $a_production = [
     'version_release'       => '0',
     'version_major'         => '82', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '0',
+    'version_patch'         => '1',
     'version_stage'         => 'a', # a=alfa,b=beta,rc=candidate,r=release
     'version_day'           => '067',
-    'version_seq'           => '1',
+    'version_seq'           => '2',
 ];
 
 return (object) [
@@ -19,8 +19,8 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'guest'                 => '0.08.0',
-    'user'                  => '0.74.0',
+    'guest'                 => '0.09.0',
+    'user'                  => '0.73.1',
     'unit'                  => '0.00.0',
     'api'                   => '0.00.0',
     'css'                   => '0.13.0',
