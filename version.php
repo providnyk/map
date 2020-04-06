e7a14c46ad60eb1de96da8cdabd2031ce1fda198
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-04-06 18:51:56 +0300

    0.83.1+a097:5 guest FIX css version
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 $a_production = [
     'version_release'       => '0',
     'version_major'         => '83', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '0',
+    'version_patch'         => '1',
     'version_stage'         => 'a', # a=alfa,b=beta,rc=candidate,r=release
     'version_day'           => '097',
-    'version_seq'           => '4',
+    'version_seq'           => '5',
 ];
 
 return (object) [
@@ -19,10 +19,10 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'guest'                 => '0.10.0',
+    'guest'                 => '0.10.1',
     'user'                  => '0.73.0',
     'unit'                  => '0.00.0',
     'api'                   => '0.00.0',
-    'css'                   => '0.13.0',
+    'css'                   => '0.14.0',
     'js'                    => '0.26.0',
 ];
