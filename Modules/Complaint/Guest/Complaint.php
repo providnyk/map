93ebd264f25d4845c0a9565fb97fbd6e22fffa5b
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-04-30 18:15:46 +0300

    0.86.0+a121:4 guest ADD move email sender from places to complaints
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- Modules/Complaint/Guest/Complaint.php
+++ Modules/Complaint/Guest/Complaint.php
@@ -7,9 +7,4 @@ use Modules\Complaint\Database\Complaint as Model;
 class Complaint extends Model
 {
 	public $translationModel = '\Modules\Complaint\Database\ComplaintTranslation';
-
-	public function getComplaints()
-	{
-		return $this->belongsTo('Modules\Building\Database\Building');
-	}
 }
--- Modules/Place/API/PlaceController.php
+++ Modules/Place/API/PlaceController.php
@@ -5,6 +5,7 @@
 namespace Modules\Place\API;
 
 #use App\Place;
+use               Modules\Complaint\Database\Complaint;
 use                        Modules\Place\API\Place;
 use                   Modules\Place\Database\Place as DBPlace;
 
@@ -37,95 +38,6 @@ class PlaceController extends Controller
 	public function destroy(DeleteRequest $request) : \Illuminate\Http\Response
 	{
 		return $this->destroyAPI($request);
-	}
-
-	public function sendIt(Array $a_places, Bool $b_dry_run = NULL) : void
-	{
-		$s_date		= date("m/d/Y");
-		$s_title	= $a_places['title'];
-		$s_address	= $a_places['address'];
-		$s_descr	= $a_places['description'];
-		$a_issues	= implode(', ', $a_places['rating']['issues']);
-
-		$a_from[0]	= [
-						's_title'		=> 'ГРОМАДСЬКА ОРГАНІЗАЦІЯ «ДЕБАТИ ЗАРАДИ ЗМІН»',
-						's_email'		=> 'debate4changes@gmail.com',
-						's_phone'		=> '+38068 255 75 91',
-						's_legal'		=> 'код: 40190259',
-						's_subject'		=> 'Заява (звернення)',
-						'a_position'	=> 'ПРЕЗИДЕНТА ГО «ДЕБАТИ ЗАРАДИ ЗМІН»',
-						'a_position_who'=> 'Президент ГО «ДЕБАТИ ЗАРАДИ ЗМІН»',
-						'a_name'		=> 'Крис Анни Сергіївни',
-						'a_name_who'	=> 'Крис Анна Сергіївна',
-						's_address'		=> 'м. Київ, вул. Інститутська, буд. 22/7, кв. 27, 01021',
-						];
-		$a_to[0]	= [
-
-						's_title'		=> 'КИЇВСЬКА МІСЬКА ДЕРЖАВНА АДМІНІСТРАЦІЯ',
-						's_address'		=> 'м. Київ, вул. Хрещатик, 36, 01044',
-						'a_email'		=> ['bogachenko.pavel@gmail.com','anna.krys.od@gmail.com',],
-#						'a_email'		=> ['zvernen@kmda.gov.ua','dsp@kmda.gov.ua',],
-						];
-		$a_to[1]	= [
-						's_title'		=> 'МІНІСТЕРСТВО СОЦІАЛЬНОЇ ПОЛІТИКИ УКРАЇНИ',
-						's_address'		=> 'м. Київ, вул. Еспланадна, 8/10, 01601',
-						'a_email'		=> ['m.d@tut.by','max.dmitriev@activelex.com',],
-#						'a_email'		=> ['info@mlsp.gov.ua','zvernennya@mlsp.gov.ua',],
-						];
-#
-		$a_params	=
-				[
-					's_date'		=> $s_date,
-					's_title'		=> $s_title,
-					's_address'		=> $s_address,
-					's_descr'		=> $s_descr,
-					'a_issues'		=> $a_issues,
-					'a_from'		=> $a_from,
-					'a_to'			=> $a_to,
-				];
-
-		$a_recipients	= [];
-		$a_recipients[]	= ['email' => config('services.mail.from'), 'name' => config('services.mail.name'),];
-
-		for ($i = 0; $i < count($a_to); $i++)
-		for ($j = 0; $j < count($a_to[$i]['a_email']); $j++)
-			$a_recipients[] = [
-						'email' => $a_to[$i]['a_email'][$j],
-						'name' => $a_to[$i]['s_title'][$j],
-					];
-
-		$s_email_from	= $a_from[0]['s_email'];
-#		$s_name_from	= $a_from[0]['s_title'];
-		$s_subj		 	= $a_from[0]['s_subject'];
-		$s_name_from	= config('services.mail.name');
-
-		if ($b_dry_run ?? FALSE)
-		{
-			$a_recipients	= [0 => ['email' => config('services.mail.from'), 'name' => config('services.mail.name')]];
-			$s_email_from	= config('services.mail.from');
-			$s_name_from	= config('services.mail.name');
-		}
-
-		for ($i = 0; $i < count($a_recipients); $i++)
-		{
-			$s_email_to = $a_recipients[$i]['email'];
-			$s_name_to = $a_recipients[$i]['name'];
-
-			Mail::send
-			(
-				'emails.complaint',
-				$a_params,
-				function($message) use ($s_subj, $s_email_to, $s_name_to, $s_email_from, $s_name_from)
-				{
-					$message
-						->from($s_email_from, $s_name_from)
-						->to($s_email_to, $s_name_to)
-						->subject($s_subj)
-					;
-				}
-			);
-
-		}
 	}
 
 	/**
@@ -165,7 +77,7 @@ class PlaceController extends Controller
 
 			if ($b_test && isset($a_places['rating']['overall']['percent']) && $a_places['rating']['overall']['percent'] < 35)
 			{
-				self::sendIt($a_places, $b_dev);
+				Complaint::sendIt($a_places, $b_dev);
 				$b_test  = FALSE;
 			}
 
