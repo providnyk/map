ccd5aaa3d041cd62eb49dde1a228fc5c7b42c629
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-05-05 17:56:03 +0300

    0.87.1+a126:3 guest FIX login button overflow
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- Modules/ProvidnykV1/Resources/views/guest/_recaptcha.blade.php
+++ Modules/ProvidnykV1/Resources/views/guest/_recaptcha.blade.php
@@ -1,4 +1,4 @@
 @if ((Auth::user() === NULL))
 								<input type="hidden" id="recap_response_{!! $id ?? '' !!}" placeholder="" name="g-recaptcha-response">
-								<div class="g-recaptcha" data-sitekey="{{ config('services.google.recaptcha.key') }}"></div>
+								<div class="g-recaptcha" style="overflow: hidden;" data-sitekey="{{ config('services.google.recaptcha.key') }}"></div>
 @endif
--- version.php
+++ version.php
@@ -3,10 +3,10 @@
 $a_production = [
     'version_release'       => '0',
     'version_leader'        => '87', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '0',
+    'version_patch'         => '1',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '126',
-    'version_seq'           => '2',
+    'version_seq'           => '3',
 ];
 
 return (object) [
@@ -19,7 +19,7 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'guest'                 => '0.14.0',
+    'guest'                 => '0.14.1',
     'user'                  => '0.73.0',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
