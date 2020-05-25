5c2835154318be99b16db321fadd6676caa42b67
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-05-25 12:19:05 +0300

    0.88.0+a146:1 guest DEL clean up
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,11 +2,11 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_leader'        => '87', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '1',
+    'version_leader'        => '88', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
+    'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '146',
-    'version_seq'           => '3',
+    'version_seq'           => '1',
 ];
 
 return (object) [
@@ -19,7 +19,7 @@ return (object) [
     . $a_production['version_day'] . ':'
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
-    'guest'                 => '0.14.1',
+    'guest'                 => '0.15.0',
     'user'                  => '0.73.0',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
