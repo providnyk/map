1f2d7c01da760d05d32632847ab7c1e9e4db01e9
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-05-25 11:32:08 +0300

    0.87.1+a146:2 guest ADD open social links in new tab
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- Modules/ProvidnykV1/Resources/views/layouts/_social_icons.blade.php
+++ Modules/ProvidnykV1/Resources/views/layouts/_social_icons.blade.php
@@ -1,5 +1,5 @@
-<a class="ico" href="https://www.facebook.com/Provodnik4u/"><img src="/{!! $theme !!}/img/soc_fb.png"  alt=""></a>
-<a class="ico" href="https://www.linkedin.com/company/provodnik/"><img src="/{!! $theme !!}/img/soc_li.png"  alt=""></a>
-<a class="ico" href="https://www.youtube.com/channel/UCCpqWOgNnsXJGiVn8uRITmQ/featured"><img src="/{!! $theme !!}/img/soc_yt.png"  alt=""></a>
-<a class="ico" href="https://www.instagram.com/provodnik4u/"><img src="/{!! $theme !!}/img/soc_in.png"  alt=""></a>
-<a class="ico" href="https://t.me/joinchat/BrZ7yFEfkm6bIoOE93U8dQ "><img src="/{!! $theme !!}/img/soc_tg.png"  alt=""></a>
+<a class="ico" target="_blank" href="https://www.facebook.com/Provodnik4u/"><img src="/{!! $theme !!}/img/soc_fb.png"  alt=""></a>
+<a class="ico" target="_blank" href="https://www.linkedin.com/company/provodnik/"><img src="/{!! $theme !!}/img/soc_li.png"  alt=""></a>
+<a class="ico" target="_blank" href="https://www.youtube.com/channel/UCCpqWOgNnsXJGiVn8uRITmQ/featured"><img src="/{!! $theme !!}/img/soc_yt.png"  alt=""></a>
+<a class="ico" target="_blank" href="https://www.instagram.com/provodnik4u/"><img src="/{!! $theme !!}/img/soc_in.png"  alt=""></a>
+<a class="ico" target="_blank" href="https://t.me/joinchat/BrZ7yFEfkm6bIoOE93U8dQ "><img src="/{!! $theme !!}/img/soc_tg.png"  alt=""></a>
--- Modules/ProvidnykV1/Resources/views/layouts/_social_networks.blade.php
+++ Modules/ProvidnykV1/Resources/views/layouts/_social_networks.blade.php
@@ -1,5 +1,5 @@
 <div class="socials">
-	<p>Contact&nbsp;us&nbsp;<a href="mailto:provodnik.team@gmail.com">provodnik.team@gmail.com</a></p>
+	<p>Contact&nbsp;us&nbsp;<a target="_blank" href="mailto:provodnik.team@gmail.com">provodnik.team@gmail.com</a></p>
 	<p>Follow us</p>
 	@include($theme . '::layouts._social_icons')
 </div>
