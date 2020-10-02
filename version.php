ddb65617f80e8801364ca0e04bd2c089fc4a6e02
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-10-02 17:52:59 +0300

    0.94.0+a276:1 user UPD clean legacy code
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -2,11 +2,11 @@
 
 $a_production = [
     'version_release'       => '0',
-    'version_leader'        => '93', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
+    'version_leader'        => '94', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
     'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
     'version_day'           => '276',
-    'version_seq'           => '2',
+    'version_seq'           => '1',
 ];
 
 return (object) [
@@ -20,7 +20,7 @@ return (object) [
     . $a_production['version_seq'],
     'release'               => $a_production['version_release'],
     'guest'                 => '0.20.0',
-    'user'                  => '0.73.0',
+    'user'                  => '0.74.0',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
     'css'                   => '0.33.0',
