a3b684d4987c1b1d14ea4cd7732ab078a9216272
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2020-11-04 20:34:36 +0200

    0.99.0+a309:1 guest ADD show directions pane at full available height
    Signed-off-by: Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>

--- version.php
+++ version.php
@@ -3,9 +3,9 @@
 $a_production = [
     'version_release'       => '0',
     'version_leader'        => '99', # changes whenever any css,js,api,guest,user value below has changed; reset each realease
-    'version_patch'         => '3',
+    'version_patch'         => '0',
     'version_maturity'      => 'a', # a=alfa,b=beta,rc=candidate,r=release,sr=service release
-    'version_day'           => '307',
+    'version_day'           => '309',
     'version_seq'           => '1',
 ];
 
@@ -23,6 +23,6 @@ return (object) [
     'user'                  => '0.79.3',
     'test'                  => '0.00.0',
     'api'                   => '0.00.0',
-    'css'                   => '0.37.0',
+    'css'                   => '0.38.0',
     'js'                    => '0.70.0',
 ];
